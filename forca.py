833fd0dce03a44d5a24fdb198a24ed44f9e76a31
DavisLira <davilirasantana86@gmail.com>
2023-10-26 11:46:12 -0300

    Adicionado a função jogar

--- forca.py
+++ forca.py
@@ -1,5 +1,9 @@
-print("*********************************")
-print("***Bem vindo ao jogo de Forca!***")
-print("*********************************")
+def jogar():    
+    print("*********************************")
+    print("***Bem vindo ao jogo de Forca!***")
+    print("*********************************")
 
-print("Fim do jogo")
+    print("Fim do jogo")
+
+if (__name__ == '__main__'):
+    jogar()
