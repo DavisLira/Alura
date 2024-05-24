e1b7fbc0e54b452085990726f3cbef779eb203f2
DavisLira <davilirasantana86@gmail.com>
2024-05-24 11:52:53 -0300

    estudo sobre doctrine no php

---
+++ Php/Doctrine_ORM/src/Helper/EntityManagerCreator.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Alura\Doctrine\Helper;
+
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+class EntityManagerCreator
+{
+    public static function createEntityManager() : EntityManager
+    {
+        $config = ORMSetup::createAttributeMetadataConfiguration(
+            paths: array(__DIR__ . "/.."),
+            isDevMode: true,
+        );
+
+        $connection = DriverManager::getConnection([
+            'driver' => 'pdo_sqlite',
+            'path' => __DIR__ . '/../../db.sqlite',
+        ], $config);
+
+        $entityManager = new EntityManager($connection, $config);
+
+        return $entityManager;
+    }
+}
