fe204c4ed42ad956328795c425bf8fda65c9a748
DavisLira <davilirasantana86@gmail.com>
2024-06-04 09:49:58 -0300

    finalização 1° curso

--- Php/Symfony/controle_series_symfony/src/Controller/SeriesController.php
+++ Php/Symfony/controle_series_symfony/src/Controller/SeriesController.php
@@ -2,21 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Series;
+use App\Repository\SeriesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SeriesController extends AbstractController
 {
-    #[Route('/series', name: 'app_series', methods: ['GET'])]
-    public function seriesListk(): Response
+    public function __construct(private SeriesRepository $seriesRepository)
     {
-        $seriesList = [
-            'Lost',
-            'Greys Anatomy',
-            'Loki',
-            'Suits',
-        ];
+        
+    }
+
+    #[Route('/series', name: 'app_series', methods: ['GET'])]
+    public function seriesList(): Response
+    {
+        $seriesList = $this->seriesRepository->findAll();
 
         return $this->render('series/index.html.twig', [
             'seriesList' => $seriesList,
@@ -27,5 +31,15 @@ class SeriesController extends AbstractController
     public function addSeriesForm(): Response
     {
         return $this->render('series/form.html.twig');
+    }
+
+    #[Route('/series/create', methods: ['POST'])]
+    public function addSeries(Request $request): Response
+    {
+        $seriesName = $request->request->get('name');
+        $series = new Series($seriesName);
+
+        $this->seriesRepository->add($series, true);
+        return new RedirectResponse('/series');
     }
 }
