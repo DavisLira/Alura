60e19f4b4bee74f371824db5ec375bbbcb661e24
DavisLira <davilirasantana86@gmail.com>
2024-06-06 11:28:11 -0300

    formulários

--- Php/Symfony/controle_series_symfony/src/Controller/SeriesController.php
+++ Php/Symfony/controle_series_symfony/src/Controller/SeriesController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Series;
+use App\Form\SeriesType;
 use App\Repository\SeriesRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,32 +22,36 @@ class SeriesController extends AbstractController
     }
 
     #[Route('/series', name: 'app_series', methods: ['GET'])]
-    public function seriesList(Request $request): Response
+    public function seriesList(): Response
     {
         $seriesList = $this->seriesRepository->findAll();
-        $session = $request->getSession();
-        $successMessage = $session->get('success');
-        $session->remove('success');
 
         return $this->render('series/index.html.twig', [
             'seriesList' => $seriesList,
-            'successMessage' => $successMessage,
         ]);
     }
 
     #[Route('/series/create', name: 'app_series_form', methods: ['GET'])]
     public function addSeriesForm(): Response
     {
-        return $this->render('series/form.html.twig');
+        $seriesForm = $this->createForm(SeriesType::class, new Series());
+        return $this->render('series/form.html.twig', compact('seriesForm'));
     }
 
     #[Route('/series/create', name: 'app_add_series', methods: ['POST'])]
     public function addSeries(Request $request): Response
     {
-        $seriesName = $request->request->get('name');
-        $series = new Series($seriesName);
-        $request->getSession()
-            ->set('success', "Série \"{$seriesName}\" adicionada com sucesso");
+        $series = new Series();
+        $seriesForm = $this->createForm(SeriesType::class, $series)
+            ->handleRequest($request);
+
+        if (!$seriesForm->isValid()) {
+            return $this->render('series/form.html.twig', compact('seriesForm'));
+        }
+        
+        $this->addFlash(
+            'success', 
+            "Série \"{$series->getName()}\" adicionada com sucesso");
 
         $this->seriesRepository->add($series, true);
         return new RedirectResponse('/series');
@@ -57,11 +62,10 @@ class SeriesController extends AbstractController
         name: 'app_delete_series', 
         methods: ['DELETE'],
         requirements: ['id' => '[0-9]+'])]
-    public function deleteSeries(int $id, Request $request): Response
+    public function deleteSeries(int $id): Response
     {
         $this->seriesRepository->removeById($id);
-        $session = $request->getSession();
-        $session->set('success', 'Série removida com sucesso');
+        $this->addFlash('success', 'Série removida com sucesso');
 
         return new RedirectResponse(url: '/series');
     }
@@ -69,14 +73,21 @@ class SeriesController extends AbstractController
     #[Route('/series/edit/{series}', name: 'app_edit_series_form', methods:['GET'])]
     public function editSeriesForm(Series $series): Response
     {
-        return $this->render('series/form.html.twig', compact('series'));
+        $seriesForm = $this->createForm(SeriesType::class, $series, ['is_edit' => true]);
+        return $this->render('series/form.html.twig', compact('seriesForm', 'series'));
     }
 
     #[Route('series/edit/{series}', name:'app_store_series_changes', methods:['PATCH'])]
     public function storeSeriesChanges(Series $series, Request $request): Response
     {
-        $series->setName($request->request->get('name'));
-        $request->getSession()->set('success', "Série \"{$series->getName()}\" editada com sucesso");
+        $seriesForm = $this->createForm(SeriesType::class, $series, ['is_edit' => true]);
+        $seriesForm->handleRequest($request);
+
+        if (!$seriesForm->isValid()) {
+            return $this->render('series/form.html.twig', compact('seriesForm', 'series'));
+        }
+        
+        $this->addFlash('success', "Série \"{$series->getName()}\" editada com sucesso");
         $this->entityManager->flush();
 
         return new RedirectResponse('/series');
