a26a03e58948106cdde6ba4db41ebee3576deb29
DavisLira <davilirasantana86@gmail.com>
2024-06-06 11:37:38 -0300

    adição de episodios e temporadas

--- Php/Symfony/controle_series_symfony/src/Entity/Series.php
+++ Php/Symfony/controle_series_symfony/src/Entity/Series.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\SeriesRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -14,6 +16,12 @@ class Series
     #[ORM\Column]
     private int $id;
 
+    /**
+     * @var Collection<int, Season>
+     */
+    #[ORM\OneToMany(targetEntity: Season::class, mappedBy: 'series', orphanRemoval: true)]
+    private Collection $seasons;
+
     
     public function __construct(
         #[ORM\Column]
@@ -21,7 +29,7 @@ class Series
         #[Assert\Length(min: 5)]
         private string $name = ''
     ) {
-        
+        $this->seasons = new ArrayCollection();
     }
 
     public function getId(): int
@@ -37,6 +45,36 @@ class Series
     public function setName(string $name): static
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Season>
+     */
+    public function getSeasons(): Collection
+    {
+        return $this->seasons;
+    }
+
+    public function addSeason(Season $season): static
+    {
+        if (!$this->seasons->contains($season)) {
+            $this->seasons->add($season);
+            $season->setSeries($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSeason(Season $season): static
+    {
+        if ($this->seasons->removeElement($season)) {
+            // set the owning side to null (unless already changed)
+            if ($season->getSeries() === $this) {
+                $season->setSeries(null);
+            }
+        }
 
         return $this;
     }
