9b974769ff0ccabaa9f074eb5b26efe75626ed5a
Davi Lira <112901144+DavisLira@users.noreply.github.com>
2024-08-16 11:55:10 -0300

    inicio do html

--- Php/Laravel/controle-series/app/Http/Controllers/SeriesController.php
+++ Php/Laravel/controle-series/app/Http/Controllers/SeriesController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    
+    public function index()
+    {
+        $series = [
+            'Punisher',
+            'Lost',
+            'Greys\'s anatomy'
+        ];
+
+        return view('listar-series', compact('series'));
+    }
 }
