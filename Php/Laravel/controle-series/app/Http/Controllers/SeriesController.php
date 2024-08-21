7fdc7e55d0bf290a6754ee3212c46da54cb6c3f9
davilirasantana86 <davilirasantana86@gmail.com>
2024-08-21 14:05:15 +0000

    função de editar

--- Php/Laravel/controle-series/app/Http/Controllers/SeriesController.php
+++ Php/Laravel/controle-series/app/Http/Controllers/SeriesController.php
@@ -23,8 +23,8 @@ class SeriesController extends Controller
 
     public function store(Request $request)
     {
-        $serie = Serie::create($request->all());
-        session()->flash('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
+        $series = Serie::create($request->all());
+        session()->flash('mensagem.sucesso', "Série '{$series->nome}' adicionada com sucesso");
 
         return to_route('series.index');
     }
@@ -33,6 +33,19 @@ class SeriesController extends Controller
     {
         $series->delete();
         session()->flash('mensagem.sucesso', "Série '{$series->nome}' removida com sucesso");
+
+        return to_route('series.index');
+    }
+
+    public function edit(Serie $series)
+    {
+        return view('series.edit')->with('serie', $series);
+    }
+
+    public function update(Serie $series, Request $request)
+    {
+        $series->fill($request->all())->save();
+        session()->flash('mensagem.sucesso', "Série '{$series->nome}' atualizada com sucesso");
 
         return to_route('series.index');
     }
