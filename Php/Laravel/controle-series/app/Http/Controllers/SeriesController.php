f66342c7b42f18616b7fd44bb73226b926e993a5
davilirasantana86 <davilirasantana86@gmail.com>
2024-08-22 14:54:41 +0000

    adição da pagina de temp

--- Php/Laravel/controle-series/app/Http/Controllers/SeriesController.php
+++ Php/Laravel/controle-series/app/Http/Controllers/SeriesController.php
@@ -29,7 +29,7 @@ class SeriesController extends Controller
         $series = Series::create($request->all());
 
         $seasons = [];
-        for ($i=1; $i <= $request->seassonsQty; $i++) { 
+        for ($i = 1; $i <= $request->seasonsQty; $i++) {
             $seasons[] = [
                 'series_id' => $series->id,
                 'number' => $i,
@@ -39,7 +39,7 @@ class SeriesController extends Controller
 
         $episodes = [];
         foreach ($series->seasons as $season) {
-            for ($j=1; $j <= $request->episodesPerSeason; $j++) { 
+            for ($j = 1; $j <= $request->episodesPerSeason; $j++) {
                 $episodes[] = [
                     'season_id' => $season->id,
                     'number' => $j,
