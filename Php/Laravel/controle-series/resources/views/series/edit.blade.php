2319d80c9218cc7202bddd4e18fe5d9141842820
davilirasantana86 <davilirasantana86@gmail.com>
2024-08-22 13:41:30 +0000

    correção

--- Php/Laravel/controle-series/resources/views/series/edit.blade.php
+++ Php/Laravel/controle-series/resources/views/series/edit.blade.php
@@ -1,5 +1,5 @@
-<x-layout title="Editar Série '{{ $serie->nome }}'">
+<x-layout title="Editar Série '{!! $serie->name !!}'">
     <a class="btn btn-dark" href="{{ route('series.index') }}">
         << Voltar</a>
-    <x-series.form :action="route('series.update', $serie->id)" :nome="$serie->nome" />
+    <x-series.form :action="route('series.update', $serie->id)" :name="$serie->name" :update="true"/>
 </x-layout>
