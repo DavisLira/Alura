409edde3c8518e252b47de1a7603b78a490e6f3a
davilirasantana86 <davilirasantana86@gmail.com>
2024-08-22 14:26:50 +0000

    criação de temporada e episódio para cada série

--- Php/Laravel/controle-series/app/Http/Controllers/SeriesController.php
+++ Php/Laravel/controle-series/app/Http/Controllers/SeriesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
+use App\Models\Episode;
+use App\Models\Season;
 use App\Models\Series;
 use Illuminate\Http\Request;
 
@@ -25,6 +27,27 @@ class SeriesController extends Controller
     public function store(SeriesFormRequest $request)
     {
         $series = Series::create($request->all());
+
+        $seasons = [];
+        for ($i=1; $i <= $request->seassonsQty; $i++) { 
+            $seasons[] = [
+                'series_id' => $series->id,
+                'number' => $i,
+            ];
+        }
+        Season::insert($seasons);
+
+        $episodes = [];
+        foreach ($series->seasons as $season) {
+            for ($j=1; $j <= $request->episodesPerSeason; $j++) { 
+                $episodes[] = [
+                    'season_id' => $season->id,
+                    'number' => $j,
+                ];
+            }
+        }
+        Episode::insert($episodes);
+
         session()->flash('mensagem.sucesso', "Série '{$series->name}' adicionada com sucesso");
 
         return to_route('series.index');
