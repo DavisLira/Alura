eb599853b727a70266302c21a7ce2e84a3959a5f
davilirasantana86 <davilirasantana86@gmail.com>
2024-09-06 13:38:27 +0000

    adição do arquivos da pasta resources

---
+++ Php/Laravel/series-control/resources/views/components/layout.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>{{ $title }} - Controle de Séries</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a href="{{ route('series.index') }}" class="navbar-brand">Home</a>
+
+            @auth
+            <form action="{{ route('logout') }}" method="post">
+                @csrf
+                <button class="btn btn-link">Sair</button>
+            </form>
+            @endauth
+
+            @if(!Route::is('login') && Auth::guest())
+            <a href="{{ route('login') }}">Entrar</a>
+            @endif
+        </div>
+    </nav>
+
+    <div class="container">
+        <h1>{{ $title }}</h1>
+
+        @isset($mensagemSucesso)
+            <div class="alert alert-success">
+                {{ $mensagemSucesso }}
+            </div>
+        @endisset
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        {{ $slot }}
+    </div>
+</body>
+</html>
---
+++ Php/Laravel/series-control/resources/views/components/series/form.blade.php
@@ -0,0 +1,24 @@
+<form action="{{ $action }}" method="POST">
+    @csrf
+
+    @if($update)
+    @method('PUT')
+    @endif
+
+    <div class="mb-3">
+        <label for="name" class="form-label">Nome:</label>
+        <input type="text" 
+                id="name" 
+                name="name" 
+                class="form-control"
+                @isset($name) value="{{ $name }}" @endisset>
+    </div>
+
+    <button type="submit" class="btn btn-primary">
+        @if (@isset($name))
+        Editar
+        @else
+        Adicionar
+        @endif
+    </button>
+</form>
