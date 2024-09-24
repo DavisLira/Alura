bb9f8c4bfb6fe1b902e5dbdeb842035f590c0ddc
Davi Lira <112901144+DavisLira@users.noreply.github.com>
2024-09-24 11:43:26 -0300

    tentativa de rota da api

--- Php/Laravel/series-control/routes/api.php
+++ Php/Laravel/series-control/routes/api.php
@@ -16,8 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
-});
+})->middleware('auth:sanctum');
 
-Route::get('/series', [SeriesController::class]);
