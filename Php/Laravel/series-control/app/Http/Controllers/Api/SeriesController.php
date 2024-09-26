56724f24a288f5d697375d4f69677b6c0dabc0c7
Davi Lira <112901144+DavisLira@users.noreply.github.com>
2024-09-26 11:52:03 -0300

    criar série com api

--- Php/Laravel/series-control/app/Http/Controllers/Api/SeriesController.php
+++ Php/Laravel/series-control/app/Http/Controllers/Api/SeriesController.php
@@ -3,13 +3,40 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Dto\SeriesData;
+use App\Http\Requests\SeriesFormRequest;
 use App\Models\Series;
+use App\Repositories\SeriesRepository;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
+    public function __construct(private SeriesRepository $seriesRepository)
+    {
+    }
+
     public function index()
     {
         return Series::all();
+    }
+
+    public function store(SeriesFormRequest $request)
+    {
+        $coverPath = null;
+
+        $seriesData = new SeriesData(
+            $request->name,
+            $request->seasonsQty,
+            $request->episodesPerSeason,
+            $coverPath
+        );
+
+        return  response()
+            ->json($this->seriesRepository->add($seriesData, 201));
+    }
+
+    public function show(Series $series)
+    {
+        return $series;
     }
 }
