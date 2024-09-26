4d20536f11f6bd73cf4d7937d93dc16681d258d0
Davi Lira <112901144+DavisLira@users.noreply.github.com>
2024-09-26 11:52:23 -0300

    ajuste no coverPath

--- Php/Laravel/series-control/app/Http/Dto/SeriesData.php
+++ Php/Laravel/series-control/app/Http/Dto/SeriesData.php
@@ -4,18 +4,20 @@ namespace App\Http\Dto;
 
 class SeriesData 
 {
-  public string $name;
-  public int $seasonsQty;
-  public int $episodesPerSeason;
-  public string $coverPath;
+    public string $name;
+    public int $seasonsQty;
+    public int $episodesPerSeason;
+    public ?string $coverPath;
 
-  // Construtor (opcional):
-
-  public function __construct(string $name, int $seasonsQty, int $episodesPerSeason, string $coverPath)
-  {
-    $this->name = $name;
-    $this->seasonsQty = $seasonsQty;
-    $this->episodesPerSeason = $episodesPerSeason;
-    $this->coverPath = $coverPath;
-  }
+    public function __construct(
+        string $name, 
+        int $seasonsQty, 
+        int $episodesPerSeason, 
+        ?string $coverPath
+    ) {
+        $this->name = $name;
+        $this->seasonsQty = $seasonsQty;
+        $this->episodesPerSeason = $episodesPerSeason;
+        $this->coverPath = $coverPath;
+    }
 }
