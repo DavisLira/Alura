12e06c4023a583faa9773ebaf557ee8b14e68840
Davi Lira <112901144+DavisLira@users.noreply.github.com>
2024-10-15 11:50:20 -0300

    php unit tests

---
+++ Php/Unit-test/src/Model/Leilao.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Alura\Leilao\Model;
+
+class Leilao
+{
+    /** @var Lance[] */
+    private $lances;
+    /** @var string */
+    private $descricao;
+
+    public function __construct(string $descricao)
+    {
+        $this->descricao = $descricao;
+        $this->lances = [];
+    }
+
+    public function recebeLance(Lance $lance)
+    {
+        if (!empty($this->lances) && $this->ehDoUltimoUsuario($lance)) {
+            return;
+        }
+
+        $this->lances[] = $lance;
+    }
+
+    /**
+     * @return Lance[]
+     */
+    public function getLances(): array
+    {
+        return $this->lances;
+    }
+
+    /**
+     * @param Lance $lance
+     * @return bool
+     */
+    private function ehDoUltimoUsuario(Lance $lance): bool
+    {
+        $ultimoLance = $this->lances[count($this->lances) - 1];
+        return $lance->getUsuario() == $ultimoLance->getUsuario();
+    }
+}
